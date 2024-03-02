5175de8196063978f7bc0593c796956cd740e5e5
root <hajimursalzade@gmail.com>
2024-03-02 16:56:02 +0000

    pleasework

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""Base Model"""
 
-import uuid
 from datetime import datetime
+import uuidi
+
 
 class BaseModel:
-    """Class"""
     def __init__(self):
-        """Initialize Instance Attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
     def __str__(self):
-        """Str Method"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
-        """Save Method"""
         self.updated_at = datetime.now()
+
     def to_dict(self):
-        """Dict Method"""
         obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict.update(__class__=self.__class__.__name__)
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
         return obj_dict
