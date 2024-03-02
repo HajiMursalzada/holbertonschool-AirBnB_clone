8a9c01a5c90c0e053ea66046ccc66e3e0c1e0d4f
root <hajimursalzade@gmail.com>
2024-03-02 11:54:26 +0000

    base model updated with kwargs and unnecessary file deleted

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,19 @@ import models
 
 class BaseModel:
     """Class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize Instance Attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                     self.__dict__[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Str Method"""
