66458b456b7aab5690ecb0788b4db343d5656894
root <hajimursalzade@gmail.com>
2024-03-02 16:42:37 +0000

    base model 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-
 """Base Model"""
 
 import uuid
 from datetime import datetime
-"""from models import storage"""
 
 class BaseModel:
     """Class"""
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Initialize Instance Attributes"""
-
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                     self.__dict__[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                else:
-                    self.__dict__[key] = value
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-           """ storage.new(self)"""
-
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
     def __str__(self):
         """Str Method"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
     def save(self):
         """Save Method"""
         self.updated_at = datetime.now()
-       """ storage.save()"""
-
     def to_dict(self):
         """Dict Method"""
         obj_dict = self.__dict__.copy()
